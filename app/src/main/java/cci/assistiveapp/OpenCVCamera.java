69b644ab89f45f9f78f9562b8778de4c20f7cffd
simon <simon_cuet@yahoo.com>
2018-10-21 21:10:33 -0500

    Edge detection on input image
    - fix screen orientation to Landscape, otherwise, openCV gives weired orientation
    - add detectEdge() method for image edge detection, no filtering is applied
    - display edge image on the ouput window
    - add new Utility package
    - add a Constants class
    
    Ref: N/A

--- app/src/main/java/cci/assistiveapp/OpenCVCamera.java
+++ app/src/main/java/cci/assistiveapp/OpenCVCamera.java
@@ -1,25 +1,30 @@
 package cci.assistiveapp;
 
+import android.content.pm.ActivityInfo;
 import android.os.Bundle;
-import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.SurfaceView;
-import android.view.View;
 import android.view.WindowManager;
 
 import org.opencv.android.BaseLoaderCallback;
 import org.opencv.android.CameraBridgeViewBase;
-import org.opencv.android.JavaCamera2View;
-import org.opencv.android.JavaCameraView;
 import org.opencv.android.LoaderCallbackInterface;
 import org.opencv.android.OpenCVLoader;
+import org.opencv.core.Core;
 import org.opencv.core.CvType;
 import org.opencv.core.Mat;
+import org.opencv.core.Size;
+import org.opencv.imgproc.Imgproc;
 
-public class OpenCVCamera extends AppCompatActivity implements CameraBridgeViewBase.CvCameraViewListener2 {
+import cci.assistiveapp.Utility.Constants;
+
+public class OpenCVCamera extends AppCompatActivity implements CameraBridgeViewBase.CvCameraViewListener2{
     private static final String TAG = "OpenCVCamera";
     private CameraBridgeViewBase cameraBridgeViewBase;
+    private Mat imgRGBA;
+    private int imgWidth, imgHeight;
+
     private BaseLoaderCallback baseLoaderCallback = new BaseLoaderCallback(this) {
         @Override
         public void onManagerConnected(int status) {
@@ -34,10 +39,11 @@ public class OpenCVCamera extends AppCompatActivity implements CameraBridgeViewB
             }
         }
     };
-    private Mat mat;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
         getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
         setContentView(R.layout.activity_opencv_camera);
         cameraBridgeViewBase = findViewById(R.id.camera_view);
@@ -47,18 +53,34 @@ public class OpenCVCamera extends AppCompatActivity implements CameraBridgeViewB
 
     @Override
     public void onCameraViewStarted(int width, int height) {
-        mat = new Mat(width, height, CvType.CV_8UC4);
+        imgWidth = width;
+        imgHeight = height;
+        imgRGBA = new Mat(width, height, CvType.CV_8UC4);
     }
 
     @Override
     public void onCameraViewStopped() {
-        mat.release();
+        imgRGBA.release();
     }
 
     @Override
     public Mat onCameraFrame(CameraBridgeViewBase.CvCameraViewFrame inputFrame) {
-        mat = inputFrame.rgba();
-        return mat;
+        imgRGBA = inputFrame.rgba();
+        Mat edgeImg = detectEdge(imgRGBA);
+        return edgeImg;
+    }
+
+    private Mat detectEdge(Mat rgbImg) {
+        Mat imgGray = new Mat(rgbImg.width(), rgbImg.height(), CvType.CV_8UC1);
+        Mat imgCanny = imgGray;
+        //convert rgb image to gray scale
+        Imgproc.cvtColor(rgbImg, imgGray, Imgproc.COLOR_RGB2GRAY);
+        //apply blur for noise removal, kernel size 3x3
+        //Imgproc.GaussianBlur(imgGray, imgGray,  new Size(5,5), 2.2,2);
+        //detect edges
+        Imgproc.Canny(imgGray, imgCanny, Constants.CANNY_THRESHOLD_LOW, Constants.CANNY_THRESHOLD_HIGH);
+
+        return imgCanny;
     }
 
     @Override
---
+++ app/src/main/java/cci/assistiveapp/Utility/Constants.java
@@ -0,0 +1,8 @@
+package cci.assistiveapp.Utility;
+
+import org.opencv.core.Size;
+
+public class Constants {
+    public static final double CANNY_THRESHOLD_LOW = 40.0;
+    public static final double CANNY_THRESHOLD_HIGH = 100.0;
+}
